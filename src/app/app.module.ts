b468d9fd28ade789c01d71741acc9a5e307899ef
sufailbadushat <sufailbadushat@gamil.com>
2022-11-25 19:57:01 +0530

    nav bar and route set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,42 @@ import { AppComponent } from './app.component';
 import { AddFriendComponent } from './add-friend/add-friend.component';
 import { ViewFriendComponent } from './view-friend/view-friend.component';
 import { SearchFriendComponent } from './search-friend/search-friend.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=
+[
+  {
+    path:"",
+    component:AddFriendComponent  
+  },
+  {
+    path:"add",
+    component:AddFriendComponent
+  },
+  {
+    path:"view",
+    component:ViewFriendComponent
+  },
+  {
+    path:"search",
+    component:SearchFriendComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AddFriendComponent,
     ViewFriendComponent,
-    SearchFriendComponent
+    SearchFriendComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
