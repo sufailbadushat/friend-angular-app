745911e7f96479d9284cb1abf77c381e510ba26e
sufailbadushat <sufailbadushat@gamil.com>
2022-11-30 10:20:36 +0530

    add frind to api and field clear

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiServicesService } from '../api-services.service';
 
 @Component({
   selector: 'app-add-friend',
@@ -11,9 +12,26 @@ export class AddFriendComponent {
   friendNickName = ""
   DescribeYourFriend = ""
 
+  constructor(private api: ApiServicesService) { }
+
   readValues = () => {
     let data: any = { "name": this.name, "friendName": this.friendName, "friendNickName": this.friendNickName, "DescribeYourFriend": this.DescribeYourFriend }
     console.log(data)
+    this.api.addFriends(data).subscribe(
+      (response: any) => {
+        console.log(response);
+        if (response.status == "success") {
+          alert("Successfully added...!");
+          this.name = ""
+          this.friendName = ""
+          this.friendNickName = ""
+          this.DescribeYourFriend = ""
+
+        } else {
+          alert("Something went wrong...!");
+        }
+      }
+    )
   }
 }
 
